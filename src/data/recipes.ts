368ac62a064d740fe6a77bc870e38eff3cafc2b3
dplaton <dplaton@gmail.com>
2020-08-21 18:28:05 +0300

    Add recipe calculator

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -16,10 +16,10 @@ const recipes: Recipe[] = [
             percentages: {
                 flour: 1,
                 liquid: 0.66,
-                yeast: 0.12,
-                salt: 0.2,
-            },
-        },
+                yeast: 0.012,
+                salt: 0.02
+            }
+        }
     },
     {
         id: 2,
@@ -29,38 +29,12 @@ const recipes: Recipe[] = [
             percentages: {
                 flour: 1,
                 liquid: 0.73,
-                yeast: 0.12,
-                salt: 0.2,
-            },
-        },
-    },
-    {
-        id: 3,
-        title: 'Ciabatta',
-        description: 'For the italian in you',
-        data: {
-            percentages: {
-                flour: 1,
-                liquid: 0.73,
-                yeast: 0.12,
-                salt: 0.2,
-            },
-        },
-    },
-    {
-        id: 4,
-        title: 'Simple bread',
-        description: 'A simple day to day bread',
-        data: {
-            percentages: {
-                flour: 1,
-                liquid: 0.66,
-                yeast: 0.12,
-                salt: 0.2,
-            },
-        },
-    },
+                yeast: 0.011,
+                salt: 0.018
+            }
+        }
+    }
 ];
 
 export const getRecipes = () => recipes;
-export const getRecipe = (id: number) => recipes.find((r) => r.id === id);
+export const getRecipe = (id: number) => recipes.find(r => r.id === id);
--- src/pages/ViewRecipe.tsx
+++ src/pages/ViewRecipe.tsx
@@ -10,7 +10,8 @@ import {
     IonLabel,
     IonList,
     IonListHeader,
-    useIonViewWillEnter,
+    IonInput,
+    useIonViewWillEnter
 } from '@ionic/react';
 import {RouteComponentProps} from 'react-router';
 
@@ -27,6 +28,30 @@ const ViewRecipe: React.FC<ViewRecipeProps> = ({match}) => {
         const rec = getRecipe(parseInt(match.params.id, 10));
         setRecipe(rec);
     });
+
+    const [amount, setAmount] = useState<number>(0);
+
+    const [values, setValues] = useState({
+        liquid: 0,
+        salt: 0,
+        yeast: 0
+    });
+
+    const handleAmountChange = (e: CustomEvent) => {
+        if (!recipe || !e.detail.value) {
+            return;
+        }
+        console.log(`Updating recipe`);
+        const {percentages} = recipe.data;
+        let grams = parseInt(e.detail.value, 10);
+
+        setAmount(grams);
+        setValues({
+            liquid: +(grams * percentages.liquid).toFixed(2),
+            salt: +(grams * percentages.salt).toFixed(2),
+            yeast: +(grams * percentages.yeast).toFixed(2)
+        });
+    };
 
     return (
         <IonPage id="view-recipe-page">
@@ -57,37 +82,59 @@ const ViewRecipe: React.FC<ViewRecipeProps> = ({match}) => {
                                 <IonItem>
                                     <IonLabel>Flour:</IonLabel>
                                     <span>
-                                        {`${
-                                            recipe.data.percentages.flour * 100
-                                        }%`}
+                                        {`${recipe.data.percentages.flour *
+                                            100}%`}
                                     </span>
                                 </IonItem>
                                 <IonItem>
                                     <IonLabel>Liquid:</IonLabel>
                                     <span>
-                                        {`${
+                                        {`${(
                                             recipe.data.percentages.liquid * 100
-                                        }%`}
+                                        ).toFixed(1)}%`}
                                     </span>
                                 </IonItem>
                                 <IonItem>
                                     <IonLabel>Salt:</IonLabel>
                                     <span>
-                                        {`${
+                                        {`${(
                                             recipe.data.percentages.salt * 100
-                                        }%`}
+                                        ).toFixed(1)}%`}
                                     </span>
                                 </IonItem>
                                 <IonItem>
                                     <IonLabel>Yeast:</IonLabel>
                                     <span>
-                                        {`${
+                                        {`${(
                                             recipe.data.percentages.yeast * 100
-                                        }%`}
+                                        ).toFixed(1)}%`}
                                     </span>
                                 </IonItem>
                             </IonList>
                         </div>
+
+                        <IonList>
+                            <IonItem>
+                                <IonLabel>Flour (g)</IonLabel>
+                                <IonInput
+                                    type="number"
+                                    placeholder="grams"
+                                    value={amount}
+                                    onIonChange={handleAmountChange}></IonInput>
+                            </IonItem>
+                            <IonItem>
+                                <IonLabel>Liquid (g)</IonLabel>
+                                <IonLabel>{values && values.liquid}</IonLabel>
+                            </IonItem>
+                            <IonItem>
+                                <IonLabel>Salt (g)</IonLabel>
+                                <IonLabel>{values && values.salt}</IonLabel>
+                            </IonItem>
+                            <IonItem>
+                                <IonLabel>Yeast (g)</IonLabel>
+                                <IonLabel>{values && values.yeast}</IonLabel>
+                            </IonItem>
+                        </IonList>
                     </>
                 )}
             </IonContent>
